dec0900bccb891d91eb7b89eb8298f3385f8143f
Sajeda Akter <sajeda16akter@gmail.com>
2023-12-24 08:56:55 +0600

    'jwt-token-complte'

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 const cors=require('cors')
 require('dotenv').config()
+var cookieParser = require('cookie-parser')
+var jwt = require('jsonwebtoken');
 const port = process.env.PORT ||5000
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}))
+app.use(cookieParser())
 
 
 
@@ -23,6 +29,22 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// middleware for jwt
+const verifyToken=(req,res,next)=>{
+  const token=req.cookies.token
+  if(!token){
+   return res.status(401).send({message:'unauthorized access'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
+    if(err){
+     return res.status(401).send({message:'Not authorized'})
+    }
+    req.user=decoded;
+    next()
+
+})
+}
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +55,19 @@ async function run() {
     const reviewsCollection=client.db('hotel_booking_DB').collection('reviews')
 
 
+    // auth related
+    app.post('/jwt',async(req,res)=>{
+      const user=req.body;
+      const token=jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+
+      res
+      .cookie('token',token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:'strict'
+      })
+      .send({suceess:true})
+    })
     // get all the rooms info
     app.get('/rooms',async(req,res)=>{
       // const price=req.query.price
@@ -63,8 +98,12 @@ async function run() {
     })
     
     // get booking data by specific email
-    app.get('/booking',async(req,res)=>{
+    app.get('/booking',verifyToken,async(req,res)=>{
       const email=req.query.email;
+     if(email !==req.user.email){
+      return res.status(403).send({message:"Forbbiden access"})
+     }
+     
       let query={}
       if(email){
         query={email:email}
