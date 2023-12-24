b74a207e4ead4045e5ceeb050ff30958c6bba5df
Sajeda Akter <sajeda16akter@gmail.com>
2023-12-24 15:52:43 +0600

    'readme'

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const port = process.env.PORT ||5000
 // middleware
 app.use(express.json())
 app.use(cors({
-  origin:['http://localhost:5173'],
+  origin:[
+    'http://localhost:5173',
+    'https://assaignment-category-0004.web.app',
+    'https://assaignment-category-0004.firebaseapp.com'
+  
+  
+  ],
   credentials:true
 }))
 app.use(cookieParser())
